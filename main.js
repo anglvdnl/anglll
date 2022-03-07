990e7818b19c216f684d4da8e0bad3003aff1698
Daniel <anglvdnl@gmail.com>
2022-03-07 19:30:06 +0100

    summ

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
-  
+
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
-  });
+});
+
+let bm = document.querySelector(".burger-menu")
+    bm.addEventListener("click", ()=>{  
+    bm.classList.toggle("burger-menu-active")
+})
